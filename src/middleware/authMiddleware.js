f183be6d22b6a7a2331305e469ec4528bcf3d36f
Łukasz Siatka <lukasz@lukaszsiatka.pl>
2019-04-01 17:17:51 -0400

    updated middleware for server and auth

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -2,6 +2,11 @@ module.exports = {
     authenticated
 };
 
-async function authenticated(){
-    return undefined;
+async function authenticated(req, res, next){
+    if(!req.session || !req.session.userID){
+        return res
+            .status(400)
+            .json({ message: 'Access denied.' });
+    }
+    next();
 }
--- src/middleware/serverMiddleware.js
+++ src/middleware/serverMiddleware.js
@@ -2,11 +2,24 @@ const express = require('express');
 const morgan = require('morgan');
 const helmet = require('helmet');
 const cors = require('cors');
+const session = require('express-session');
+
+require('dotenv').config();
 
 module.exports = (app) => {
     app.use(express.json());
     app.use(express.urlencoded({ extended: false }));
     app.use(morgan('dev'));
     app.use(helmet());
+    app.use(session({
+        name: 'sessionName',
+        secret: process.env.SECRET_KEY || 'secretKey',
+        cookie: {
+            maxAge: 24*3600000
+        },
+        resave: false,
+        saveUninitialized: false,
+        httpOnly: true
+    }));
     app.use(cors());
 }
