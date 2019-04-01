2fa51a6a4d90c2f6a525792c3445f91d1b91c012
Łukasz Siatka <lukasz@lukaszsiatka.pl>
2019-04-01 16:53:13 -0400

    added default api route

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,4 +6,11 @@ const auth = require('./auth');
 router.use('/', auth);
 router.use('/users', users);
 
+router.route('/')
+    .get(async (req, res) => {
+        res.json({
+            message: "API works"
+        });
+    });
+
 module.exports = router;
