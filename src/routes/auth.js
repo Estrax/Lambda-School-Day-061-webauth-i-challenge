54011c36f6cec3d70d854df5b6c14c2e7ea472d3
Łukasz Siatka <lukasz@lukaszsiatka.pl>
2019-04-01 16:59:18 -0400

    added register route

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,8 +1,23 @@
 const router = require('express').Router();
+const db = require('../database/helpers/helpers');
+const bcrypt = require('bcryptjs');
 
 router.route('/register')
     .post(async (req, res) => {
-
+        let user = req.body;
+        user.password = bcrypt.hashSync(user.password, 20);
+        await db
+                .addUser(user)
+                .then(id => {
+                    res
+                        .status(201)
+                        .json({ id });
+                })
+                .catch(err => {
+                    res
+                        .status(500)
+                        .json("There was an error saving user to the database!");
+                });
     });
 
 router.route('/login')
